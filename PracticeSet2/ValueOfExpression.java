70ff87517361692859022447bcedfcb5d165dafd
trivediSK <satyaatwork007@gmail.com>
2025-06-18 14:00:00 +0530

    BODMAS in java

---
+++ PracticeSet2/ValueOfExpression.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ValueOfExpression{
+    public static void main(String[] args){
+        float a = 12/4 * 12/2;
+
+        //Works on the basis of Precedence and Associativity(Left to Right or Right to Left)
+
+        System.out.printf("result is %f", a);
+
+    }
+}
